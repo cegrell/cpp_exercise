d58ccdbd8deb7e548a4f4dcfe39f902c6e0f9079
cegrell <niklashojman@gmail.com>
2021-10-31 17:06:00 +0000

    Variable Sized Arrays

--- RemoveElements/RemoveElements.cpp
+++ RemoveElements/RemoveElements.cpp
@@ -1,20 +1,25 @@
-// RemoveElements.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    auto removeElement(std::vector<int>& nums, int val) {
+        // This will return a iterator
+        auto it = std::remove_if(nums.begin(), nums.end(), [=](int num) {return num == val; });
+
+        // The result of subtracting one iterator fr another is to get an int
+        return it;// -nums.begin();
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Solution s;
+    std::vector<int> nums = { 3, 2, 3, 2 };
+    int rm_int = 3;
+    auto a = s.removeElement(nums, rm_int);
+
+    //for (char i: a)
+    //    std::cout << i << std::endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ VariableSizedArrays/VariableSizedArrays.cpp
@@ -0,0 +1,46 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+
+int main() {
+    int n;
+    int q;
+    std::cin >> n;
+    std::cin >> q;
+
+    std::vector<std::vector<int>> vect;
+    int num_elements;
+    int tmp;
+    for (int i = 0; i < n; i++)
+    {
+        
+        std::cin >> num_elements;
+        std::vector<int> row_v;
+
+        for (int j = 0; j < num_elements; j++)
+        {
+            
+            std::cin >> tmp;
+            row_v.push_back(tmp);
+        }
+
+        vect.push_back(row_v);
+    }
+
+    // Queries
+    std::vector<int> q_vect;
+    int row;
+    int col;
+    for (int i = 0; i < q; i++)
+    {
+        std::cin >> row;
+        std::cin >> col;
+
+        std::cout << vect[row][col] << std::endl;        
+    }
+
+    return 0;
+}
